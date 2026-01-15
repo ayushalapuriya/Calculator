59cfb04a7dcd7adaa88ffddd4d0dfdf73fcd77d5
Ayush Alapuriya <alapuriya0@gmail.com>
2026-01-15 22:36:42 +0530

    [Ayush Alapuriya] Add. Initial calculator setup with non-static logic

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public void start(){
+        System.out.println("Calculator start");
+    }
+    public static void main(String[] args) {
+        Calculator calc=new Calculator();
+        calc.start();
+    }
+}
